735256ea2f1fb35dbaf7d41819d8049ee7a78c63
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2022-05-20 14:38:27 +0530

    Update check_user_logged_in.dart

--- lib/utils/check_user_logged_in.dart
+++ lib/utils/check_user_logged_in.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:m0vieapp/screens/login_screen.dart';
 import 'package:m0vieapp/screens/tab_activity.dart';
-import 'package:m0vieapp/screens/tabs/home_screen.dart';
 
 class CheckUserLoggedIn extends StatefulWidget {
   const CheckUserLoggedIn({Key? key}) : super(key: key);
