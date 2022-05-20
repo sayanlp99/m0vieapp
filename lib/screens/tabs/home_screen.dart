a8463fde830f35031f456f819a82bf00c01995ce
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2022-05-20 17:00:18 +0530

    Minor fixes

--- lib/screens/tabs/home_screen.dart
+++ lib/screens/tabs/home_screen.dart
@@ -35,84 +35,82 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
-        child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                children: [
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Hi,',
-                        style: TextStyle(
-                          fontSize: MediaQuery.textScaleFactorOf(context) * 35,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  CircleAvatar(
-                    radius: 20,
-                    backgroundImage: NetworkImage(
-                      FirebaseAuth.instance.currentUser!.photoURL.toString(),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                ],
-              ),
-              Row(children: [
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
                 const SizedBox(
                   width: 16,
                 ),
-                Text(
-                  'Coming Soon',
-                  style: TextStyle(
-                    fontSize: MediaQuery.textScaleFactorOf(context) * 20,
-                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hi,',
+                      style: TextStyle(
+                        fontSize: MediaQuery.textScaleFactorOf(context) * 35,
+                      ),
+                    ),
+                  ],
                 ),
                 const Spacer(),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.chevron_right_sharp),
+                CircleAvatar(
+                  radius: 20,
+                  backgroundImage: NetworkImage(
+                    FirebaseAuth.instance.currentUser!.photoURL.toString(),
+                  ),
                 ),
                 const SizedBox(
                   width: 16,
                 ),
-              ]),
-              comingSoonLoaded
-                  ? const CircularProgressIndicator()
-                  : Container(
-                      height: 200,
-                      width: MediaQuery.of(context).size.width,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-                        itemCount: comingSoonItem!.length,
-                        itemBuilder: (context, index) {
-                          return ComingSoonCards(
-                            title: comingSoonItem![index].title,
-                            img: comingSoonItem![index].image,
-                            releaseDate: comingSoonItem![index].releaseState +
-                                ',' +
-                                comingSoonItem![index].year,
-                            stars: comingSoonItem![index].stars,
-                            id: comingSoonItem![index].id,
-                          );
-                        },
-                      ),
+              ],
+            ),
+            Row(children: [
+              const SizedBox(
+                width: 16,
+              ),
+              Text(
+                'Coming Soon',
+                style: TextStyle(
+                  fontSize: MediaQuery.textScaleFactorOf(context) * 20,
+                ),
+              ),
+              const Spacer(),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.chevron_right_sharp),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+            ]),
+            comingSoonLoaded
+                ? const CircularProgressIndicator()
+                : SizedBox(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: comingSoonItem!.length,
+                      itemBuilder: (context, index) {
+                        return ComingSoonCards(
+                          title: comingSoonItem![index].title,
+                          img: comingSoonItem![index].image,
+                          releaseDate: comingSoonItem![index].releaseState +
+                              ',' +
+                              comingSoonItem![index].year,
+                          stars: comingSoonItem![index].stars,
+                          id: comingSoonItem![index].id,
+                        );
+                      },
                     ),
-            ],
-          ),
+                  ),
+          ],
         ),
       ),
     );
