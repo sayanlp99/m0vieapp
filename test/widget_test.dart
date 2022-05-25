fac41a351a423904cea1469854761be074444fc5
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2022-05-25 16:11:50 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:m0vieapp/main.dart';
 
 void main() {
-  runApp(const MyApp());
+  testWidgets('MyApp smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    // Add tests here!
+  });
 }
