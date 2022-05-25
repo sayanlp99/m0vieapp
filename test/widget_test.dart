b43e8b389bdaaf2c5a80569c767d01a5228aa96c
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2022-05-25 16:22:36 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:m0vieapp/main.dart';
 
 void main() {
   testWidgets('MyApp smoke test', (WidgetTester tester) async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
     await tester.pumpWidget(const MyApp());
     // Add tests here!
   });
